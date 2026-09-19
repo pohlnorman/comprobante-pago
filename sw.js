c971fd10536c258e44fe248625eee70f6b0230ab
pohlnorman <82533266+pohlnorman@users.noreply.github.com>
2026-09-18 21:37:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "comprobante-pago-v2";
+const CACHE_NAME = "comprobante-pago-v3";
 
 const ARCHIVOS = [
     "./",
@@ -11,18 +11,57 @@ const ARCHIVOS = [
 ];
 
 self.addEventListener("install", function (event) {
+
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll(ARCHIVOS);
         })
     );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (nombres) {
+
+            return Promise.all(
+
+                nombres.map(function (nombre) {
+
+                    if (nombre !== CACHE_NAME) {
+                        return caches.delete(nombre);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
 });
 
 
 self.addEventListener("fetch", function (event) {
+
     event.respondWith(
+
         caches.match(event.request).then(function (respuesta) {
-            return respuesta || fetch(event.request);
+
+            if (respuesta) {
+                return respuesta;
+            }
+
+            return fetch(event.request);
+
         })
+
     );
+
 });
